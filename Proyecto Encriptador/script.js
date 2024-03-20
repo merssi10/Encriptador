cd21cb5df844bbc6bce5b5bddf7a909a03c8b9ec
Mervis Bolivar <159503065+merssi10@users.noreply.github.com>
2024-03-19 20:55:01 -0500

    Add files via upload

---
+++ Proyecto Encriptador/script.js
@@ -0,0 +1,44 @@
+const campo_texto = document.querySelector("#texto-encriptado");
+const campo_mensaje = document.querySelector("#campo-mensaje");
+
+const matriz_code = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a","ai"],
+    ["o","ober"],
+    ["u","ufat"]
+]; 
+
+function btnEncriptar(){
+    const texto = encriptar(campo_texto.value);
+    campo_mensaje.value = texto;
+}
+
+function encriptar(fraseEncriptada) {
+    for(let i = 0; i < matriz_code.length; i++) {
+        if(fraseEncriptada.includes(matriz_code[i][0])){
+            fraseEncriptada = fraseEncriptada.replaceAll(
+                matriz_code[i][0],  
+                matriz_code[i][1]
+            );
+        }
+}
+return fraseEncriptada;
+}
+
+function btnDesencriptar(){
+    const texto = desencriptar(campo_texto.value);
+    campo_mensaje.value = texto;
+}
+
+function desencriptar(fraseDesencriptada) {
+    for(let i = 0; i < matriz_code.length; i++) {
+        if(fraseDesencriptada.includes(matriz_code[i][1])){
+            fraseDesencriptada = fraseDesencriptada.replaceAll(
+                matriz_code[i][1],  
+                matriz_code[i][0]
+            );
+        }
+    }
+    return fraseDesencriptada;
+}
